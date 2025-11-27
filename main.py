3ba8d8251f2f2e8cdd2d54185dcf21d0f337dad3
Udhayan2004 <udhayannawin@gmail.com>
2025-11-27 22:50:25 +0530

    Learning fastapi

--- main.py
+++ main.py
@@ -13,3 +13,20 @@ def about():
     return {"data":{
         "About":"This is a sample FastAPI application."
     }}
+
+#dynamic route
+@app.get("/blog/{id}")
+def blog(id:str):
+    return {"Blog ID":int(id)}
+#query parameters
+@app.get("/blog")
+# if pass a value as an argument it will give the output otherwise it will shows the error
+def blog_list(limit=0,published:bool =True):
+    if published:
+        return {"data":f"{limit} published blogs from the database"}
+    else:
+        return {"data":f"{limit} blogs from the database"}
+
+@app.get("blog/unpublished")
+def unpublished():
+    return {"data":"All unpublished blogs"}
